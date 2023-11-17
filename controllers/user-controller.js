af4565241c3d1670b406c6fb8763e5d5c7b88c30
David Sanjaime <dsanjaime2002@gmail.com>
2023-11-17 18:48:22 +0100

    cambio

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -58,12 +58,11 @@ const addUser = async (req,res) => {
 
 const updateUserByIdImage = async (req,res) => {
   const userId = req.params.id;
-  //const updatedUser = JSON.parse(req.body.user)
-  const updatedUser = req.body;
+  const updatedUser = JSON.parse(req.body.user)
+  //const updatedUser = req.body;
   const image = req.files.image[0].location;
   updatedUser.image = image;
   
-
   try {
     const updatedUser = await User.findByIdAndUpdate(userId, updatedUser, { new: true });
     if (!updatedUser) {
